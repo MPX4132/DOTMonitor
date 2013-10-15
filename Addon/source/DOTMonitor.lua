c3e3fb6a986568a97cef2ecc0f8d4f3264593042
Matias Barcenas <Matias.Barcenas@Gmail.com>
2013-10-14 23:15:05 -0500

    Added Graphics
    Added border graphics which signal the spell is about to run out.

--- Addon/source/DOTMonitor.lua
+++ Addon/source/DOTMonitor.lua
@@ -1,5 +1,10 @@
 DOTMonitor = {debugMode = false} -- Main Addon
 
+local iconIntensity = function(magnitude)
+	BorderTheme = {"Interface\\AddOns\\DOTMonitor\\graphics\\icon_border_white", "Interface\\AddOns\\DOTMonitor\\graphics\\icon_border_gloss_orange"}
+	return BorderTheme[((magnitude >= 0.95 and 2) or 1)]
+end
+
 DOTMonitor.scanner = {
 	debuffMonitor = (function(self, elapsed)
 		self.lastUpdate = self.lastUpdate and (self.lastUpdate + elapsed) or 0
@@ -17,10 +22,16 @@ DOTMonitor.scanner = {
 			if caster == "player" then
 				local timeRemaining = (expiration - GetTime())
 				local timeFraction 	= (duration ~= 0) and (timeRemaining / duration) or 0
-
-				self:SetHeight(spellIconSize - (timeFraction * spellIconSize))
-				self:SetWidth(spellIconSize - (timeFraction * spellIconSize))
-				self:SetAlpha(spellMaxAlpha	- (timeFraction * spellMaxAlpha))
+				local sizeMagnitude = spellIconSize 	- (timeFraction * spellIconSize)
+				local alphaMagnitude = spellMaxAlpha 	- (timeFraction * spellMaxAlpha)
+				
+				self:SetHeight(sizeMagnitude)
+				self:SetWidth(sizeMagnitude)
+				self:SetAlpha(alphaMagnitude)
+				
+				
+				
+				self.border:SetTexture(iconIntensity(alphaMagnitude))
 			else
 				self:SetHeight(spellIconSize)
 				self:SetWidth(spellIconSize)
